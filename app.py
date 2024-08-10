3526277a7c764d95fe3ae439b2c2fd44e0a300e0
wlstmd <jinwoo8288@gmail.com>
2024-08-10 23:09:34 +0900

    pylint pytest..

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+"""
+learn-github-actions app demo code
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """
+    hello world api
+    """
     return '<p>Hello, World!!</p>'
 
 if __name__ == '__main__':
